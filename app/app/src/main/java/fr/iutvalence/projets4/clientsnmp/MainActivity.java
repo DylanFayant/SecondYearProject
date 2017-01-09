e68312839cdb5b8ba4e2cf162b46048ab931f6dc
dylan.fayant <dylan.fayant@iut-valence.fr>
2017-01-09 09:33:37 +0100

    Essai

--- app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MainActivity.java
+++ app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // coucou
+        super.onCreate(savedInstanceState); // coucou test
         setContentView(R.layout.activity_main);
     }
 }
