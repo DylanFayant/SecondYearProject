e80fd182fcfd54e82b9df55d8edd44c3af3e7fe3
foexs <tapaleurre@hotmail.fr>
2017-01-09 15:33:25 +0100

    Tentative de création de manager

--- app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MainActivity.java
+++ app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MainActivity.java
@@ -1,13 +1,52 @@
 package fr.iutvalence.projets4.clientsnmp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.snmp4j.smi.OID;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Manager manager = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // coucou test
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    protected void start(View view) {
+        this.manager = new Manager("udp:127.0.0.1/161");
+        String sysDescr = null;
+        try {
+            sysDescr = manager.getAsString(new OID(".1.3.6.1.2.1.1.1.0"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sysDescr);
+    }
+
 }
