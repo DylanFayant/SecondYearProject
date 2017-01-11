d9c5b4f2fd017ca715dd730ced342fa06d11e9da
foexs <tapaleurre@hotmail.fr>
2017-01-11 11:39:15 +0100

    Service bieng made

--- app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/AgentService.java
+++ app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/AgentService.java
@@ -1,94 +1,25 @@
 package fr.iutvalence.projets4.clientsnmp;
 
-/***
- Copyright (c) 2008-2012 CommonsWare, LLC
- Licensed under the Apache License, Version 2.0 (the "License"); you may not
- use this file except in compliance with the License. You may obtain	a copy
- of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
- by applicable law or agreed to in writing, software distributed under the
- License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
- OF ANY KIND, either express or implied. See the License for the specific
- language governing permissions and limitations under the License.
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
- From _The Busy Coder's Guide to Android Development_
- http://commonsware.com/Android
- */
-        import android.app.Notification;
-        import android.app.NotificationManager;
-        import android.app.PendingIntent;
-        import android.app.Service;
-        import android.content.Intent;
-        import android.os.Build;
-        import android.os.IBinder;
-        import android.util.Log;
 
-public class AgentService extends Service {
-    public static final String EXTRA_PLAYLIST="EXTRA_PLAYLIST";
-    public static final String EXTRA_SHUFFLE="EXTRA_SHUFFLE";
-    private boolean isPlaying=false;
+public class AgentService extends IntentService {
 
-    private int NOTIFICATION_ID=11;
-
-    Notification notification;
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        String playlist=intent.getStringExtra(EXTRA_PLAYLIST);
-        boolean useShuffle=intent.getBooleanExtra(EXTRA_SHUFFLE, false);
-
-        play(playlist, useShuffle);
-
-        return(START_NOT_STICKY);
+    public AgentService(){
+        super(AgentService.class.getName());
     }
 
     @Override
-    public void onDestroy() {
-        stop();
-    }
+    protected void onHandleIntent(Intent workIntent) {
+        // Gets data from the incoming Intent
+        //String dataString = workIntent.getDataString();
+        // Do work here, based on the contents of dataString
+        Log.d("Test","Service lancé");
+        //Log.d("Test","Donnée:"+dataString);
+        Toast.makeText(this, "Bijour", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return(null);
-    }
-
-    NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-    private void play(String playlist, boolean useShuffle) {
-        if (!isPlaying) {
-            Log.w(getClass().getName(), "Got to play()!");
-            isPlaying=true;
-
-            Intent i=new Intent(this, ServiceActivity.class);
-            Notification.Builder builder = new Notification.Builder(this);
-            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
-                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, i, 0);
-
-            builder.setAutoCancel(false);
-            builder.setTicker("this is ticker text");
-            builder.setContentTitle("Service is running");
-            builder.setContentText("Click");
-            builder.setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_disabled); // ICONE DE NOTIF
-            builder.setContentIntent(pendingIntent);
-            builder.setOngoing(true);
-
-            if (Build.VERSION.SDK_INT < 16) {
-                notification = builder.getNotification();
-            }else {
-               notification = builder.build();
-            }
-
-
-
-            startForeground(NOTIFICATION_ID, notification);
-        }
-    }
-
-    private void stop() {
-        if (isPlaying) {
-            Log.w(getClass().getName(), "Got to stop()!");
-            isPlaying=false;
-            stopForeground(true);
-        }
     }
 }
--- app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/ServiceActivity.java
+++ app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/ServiceActivity.java
@@ -2,27 +2,30 @@ package fr.iutvalence.projets4.clientsnmp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class ServiceActivity extends Activity {
+    Intent intent;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        this.intent =new Intent(ServiceActivity.this, AgentService.class);
+        //intent.setData(Uri.parse("Donnée"));
     }
 
     public void StartSvc(View v) {
-        Intent i=new Intent(this, AgentService.class);
-
-        i.putExtra(AgentService.EXTRA_PLAYLIST, "activity_service");
-        i.putExtra(AgentService.EXTRA_SHUFFLE, true);
-        Log.d("Test:","Entré dans startsvc");
-        startService(i);
+        Log.d("Test","Appuyé sur le bouton start");
+        this.startService(intent);
     }
 
     public void StopSvc(View v) {
         stopService(new Intent(this, AgentService.class));
+        Log.d("Test","Appuyé sur le bouton stop");
+        this.stopService(intent);
     }
 }
