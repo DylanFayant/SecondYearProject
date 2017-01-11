3e942d99b28ea025dd9f6a5bf3b4c21fadc078a5
dylan.fayant <dylan.fayant@iut-valence.fr>
2017-01-11 17:21:26 +0100

    Add srvc

--- app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MIB/SrvcTMMOpen.java
+++ app/app/src/main/java/fr/iutvalence/projets4/clientsnmp/MIB/SrvcTMMOpen.java
@@ -2,17 +2,19 @@ package fr.iutvalence.projets4.clientsnmp.MIB;
 
 import android.app.ActivityManager;
 
+import com.jaredrummler.android.processes.AndroidProcesses;
+
 import org.snmp4j.smi.OID;
 
-import fr.iutvalence.projets4.clientsnmp.MainActivity;
+import java.util.List;
 
-import static android.content.Context.ACTIVITY_SERVICE;
+import fr.iutvalence.projets4.clientsnmp.MainActivity;
 
 /**
  * Created by ethis on 09/01/17.
  */
 
-public class SrvcTMMOpen implements MIBComposite, MIBElement<String> {
+public class SrvcTMMOpen implements MIBComposite, MIBElement<Boolean> {
 
     public final static String TMM_PACKAGE = "fr.iutvalence.fayantd.braintrainer";
 
@@ -27,12 +29,15 @@ public class SrvcTMMOpen implements MIBComposite, MIBElement<String> {
     }
 
     @Override
-    public String getValue() {
-        ActivityManager am = (ActivityManager) MainActivity.getContext().getSystemService(ACTIVITY_SERVICE);
-        // The first in the list of RunningTasks is always the foreground task.
-        ActivityManager.RunningTaskInfo foregroundTaskInfo = am.getRunningTasks(1).get(0);
+    public Boolean getValue() {
 
-        String foregroundTaskPackageName = foregroundTaskInfo .topActivity.getPackageName();
-        return foregroundTaskPackageName;
+        List<ActivityManager.RunningAppProcessInfo> processes = AndroidProcesses.getRunningAppProcessInfo(MainActivity.getContext());
+
+        for (int i = 0; i < processes.size(); i++)
+        {
+            if(processes.get(i).processName.equals(TMM_PACKAGE))
+                return true;
+        }
+        return false;
     }
 }
